ae48dda2bc9f392d3e5a77a7db753a4e50fbc7e4
developerbayazid <bayazid.freelancer@gmail.com>
2026-02-09 01:05:53 +0600

    first api route

---
+++ app/Http/Controllers/API/TestApiController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TestApiController extends Controller
+{
+    public function test()
+    {
+        return "Api Worked";
+    }
+}
