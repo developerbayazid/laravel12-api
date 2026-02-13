c9dc3b4bf7fcc5dd5aac1a0a41f461cceae381b4
developerbayazid <bayazid.freelancer@gmail.com>
2026-02-13 20:56:52 +0600

    feat(students): add index API to fetch all students

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\StudentApiController;
 use App\Http\Controllers\API\TestApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,3 +11,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/test', [TestApiController::class, 'test'])->name('test-api');
+
+
+Route::apiResource('/students', StudentApiController::class);
